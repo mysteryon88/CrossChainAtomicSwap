f5645e64bb8590e5182b8a1f01b30ec4a5496238
Sergey <mysteryon88@yandex.ru>
2024-04-19 18:07:32 +0300

    update cross-chain native tokens swap

---
+++ contracts/Native.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Native {
+    address public immutable owner;
+    address public immutable otherParty;
+    uint public immutable deadline;
+    bytes32 public immutable hashKey;
+
+    event Swap(string indexed key);
+
+    constructor(address _otherParty, uint _deadline, bytes32 _hashKey) payable {
+        owner = msg.sender;
+        otherParty = _otherParty;
+        deadline = _deadline;
+        hashKey = _hashKey;
+    }
+
+    function confirmSwap(string calldata _key) external {
+        require(
+            msg.sender == otherParty,
+            "Only the other party can confirm the deposit"
+        );
+        require(
+            hashKey == keccak256(abi.encodePacked(_key)),
+            "The key does not match the hash"
+        );
+
+        emit Swap(_key);
+        payable(otherParty).transfer(address(this).balance);
+    }
+
+    function withdrawal() external {
+        require(block.timestamp > deadline, "Swap not yet expired");
+        payable(owner).transfer(address(this).balance);
+    }
+}
---
+++ test/Native.test.ts
@@ -0,0 +1,100 @@
+import {
+  time,
+  loadFixture,
+} from "@nomicfoundation/hardhat-toolbox/network-helpers";
+import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
+import { expect } from "chai";
+import hre from "hardhat";
+
+function generateRandomHexString(length: number) {
+  let result = "";
+  const characters = "0123456789abcdef";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const timeout = 600;
+// How much should A fix in the contract
+const amountA = 1000;
+// How much should B fix in the contract
+const amountB = 10000;
+
+// A swaps 1000 native tokens of network A for 10000 native tokens of network B
+describe("Cross-Chain Atomic Swap Native Tokens", function () {
+  async function deployA() {
+    const [partyA, partyB, partyC] = await hre.ethers.getSigners();
+
+    // A and B chose a single exchange time
+    const deadline = (await time.latest()) + timeout;
+
+    // A generates a key
+    const keyA = generateRandomHexString(64);
+    const hashKeyA = hre.ethers.keccak256(hre.ethers.toUtf8Bytes(keyA));
+
+    // A created a contract by fixing the time of execution
+    // Also the tokens are locked with key A
+    const NativeA = await hre.ethers.getContractFactory("Native", {
+      signer: partyA,
+    });
+    const nativeA = await NativeA.deploy(partyB.address, deadline, hashKeyA, {
+      value: amountA,
+    });
+
+    return { nativeA, partyA, partyB, keyA, hashKeyA, deadline };
+  }
+
+  it("Good Swap", async function () {
+    const { nativeA, partyA, partyB, keyA, hashKeyA, deadline } =
+      await loadFixture(deployA);
+
+    // After A has created a contract, B checks the balance and deploys its
+    // where tokens are locked with key A
+
+    // B created a contract by fixing the time of execution
+
+    const NativeB = await hre.ethers.getContractFactory("Native", {
+      signer: partyB,
+    });
+    const nativeB = await NativeB.deploy(partyA.address, deadline, hashKeyA, {
+      value: amountB,
+    });
+
+    // A checks the contract B
+    // If A is satisfied, he takes the funds from B's contract and publishes the key
+
+    await expect(nativeB.connect(partyA).confirmSwap(keyA))
+      .to.emit(nativeB, "Swap")
+      .withArgs(keyA);
+
+    // B sees the key in the contract events and opens contract A
+    await expect(
+      nativeA.connect(partyB).confirmSwap(keyA)
+    ).to.changeEtherBalance(partyB, amountA);
+  });
+
+  it("Successful withdrawal", async function () {
+    const { nativeA, partyA, deadline } = await loadFixture(deployA);
+
+    // B has not deployed his contract, after the deadline A can withdraw funds
+    await time.increaseTo(deadline);
+
+    await expect(nativeA.connect(partyA).withdrawal()).to.changeEtherBalance(
+      partyA,
+      amountA
+    );
+  });
+
+  it("Unsuccessful withdrawal", async function () {
+    const { nativeA, partyA, deadline } = await loadFixture(deployA);
+
+    // B has not deployed his contract, after the deadline A can withdraw funds
+    //  await time.increaseTo(deadline);
+
+    await expect(nativeA.connect(partyA).withdrawal()).to.be.revertedWith(
+      "Swap not yet expired"
+    );
+  });
+});
