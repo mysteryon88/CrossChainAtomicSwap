9c0a6a0d9d285a8489cb6b1165e02e5d62866807
Sergey <mysteryon88@yandex.ru>
2024-04-19 23:21:51 +0300

    update import

--- contracts/ERC20Swap.v1.sol
+++ contracts/ERC20Swap.v1.sol
@@ -1,17 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-interface IERC20 {
-    function transfer(address to, uint256 amount) external returns (bool);
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) external returns (bool);
-
-    function balanceOf(address account) external view returns (uint256);
-}
+import "./interfaces/IERC20.sol";
 
 contract ERC20SwapTransferFrom {
     address public immutable owner;
